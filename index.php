344b1ed0db43ab3cc2acfc03854ecbe733f01b75
Michael Scocco <scocco.michael87@gmail.com>
2021-06-24 16:08:50 +0200

    database

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$db =
+        [
+            [
+                "poster" => "",
+                "title" => "New Jersey",
+                "author" => "Bon Jovi",
+                "genre" => "Rock",
+                "year" => "1988"
+            ],
+            [
+                "poster" => "",
+                "title" => "Live at Wembley 86",
+                "author" => "Queen",
+                "genre" => "Pop",
+                "year" => "1992"
+            ],
+            [
+                "poster" => "",
+                "title" => "Ten's Summoner's Tales",
+                "author" => "Sting",
+                "genre" => "Pop",
+                "year" => "1993"
+            ],
+            [
+                "poster" => "",
+                "title" => "Steve Gadd Band",
+                "author" => "Steve Gadd Band",
+                "genre" => "Jazz",
+                "year" => "2018"
+            ],
+            [
+                "poster" => "",
+                "title" => "Brave new World",
+                "author" => "Iron Maiden",
+                "genre" => "Metal",
+                "year" => "2000"
+            ],
+            [
+                "poster" => "",
+                "title" => "One more car, one more raider",
+                "author" => "Eric Clapton",
+                "genre" => "Rock",
+                "year" => "2002"
+            ],
+            [
+                "poster" => "",
+                "title" => "Made in Japan",
+                "author" => "Deep Purple",
+                "genre" => "Rock",
+                "year" => "1972"
+            ],
+            [
+                "poster" => "",
+                "title" => "And Justice for All",
+                "author" => "Metallica",
+                "genre" => "Metal",
+                "year" => "1988"
+            ],
+            [
+                "poster" => "",
+                "title" => "Hard Wired",
+                "author" => "Dave Weckl",
+                "genre" => "Jazz",
+                "year" => "1994"
+            ],
+            [
+                "poster" => "",
+                "title" => "Bad",
+                "author" => "Michael Jacjson",
+                "genre" => "Pop",
+                "year" => "1987"
+            ]
+        ];
+?>
+</body>
+</html>
